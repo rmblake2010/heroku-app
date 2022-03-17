fdd274dcd3d40de7d59607978e77a60089142f9f
Ryan Blake <xbungalowx@gmail.com>
2022-03-16 19:44:30 -0500

    fix port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
